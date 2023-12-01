35952b470ca2cb11461d2e4f674561ba6f9b2c85
artem9g <98230920+artem9g@users.noreply.github.com>
2023-12-01 23:14:02 +0200

    121

--- js/app.js
+++ js/app.js
@@ -13,13 +13,6 @@
             document.documentElement.classList.add(className);
         }));
     }
-    function addLoadedClass() {
-        if (!document.documentElement.classList.contains("loading")) window.addEventListener("load", (function() {
-            setTimeout((function() {
-                document.documentElement.classList.add("loaded");
-            }), 0);
-        }));
-    }
     let _slideUp = (target, duration = 500, showmore = 0) => {
         if (!target.classList.contains("_slide")) {
             target.classList.add("_slide");
@@ -4133,7 +4126,6 @@
     }));
     window["FLS"] = true;
     isWebp();
-    addLoadedClass();
     menuInit();
     spollers();
     headerScroll();
